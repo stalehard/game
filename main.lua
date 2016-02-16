7bb57b287df8224decd9988470e133b2c81a8b2f
Andrey Ivanov <lekkion@gmail.com>
2016-02-16 18:14:12 +0400

    first

---
+++ main.lua
@@ -0,0 +1,6 @@
+playerImg = nil -- this is just for storage
+
+function love.load(arg)
+    playerImg = love.graphics.newImage('res/plane.png')
+    --we now have an asset ready to be used inside Love
+end
